b21df1696f2d40de2516d66fdb7600b13a176270
Oleg Konyk <okonyk@apple.com>
2018-06-05 08:14:55 -0700

    remove dist folder; .gitignore added

--- dist/book.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYm9vay5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uL3NyYy9ib29rLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiIifQ==
--- dist/employes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Employes = /** @class */ (function () {
-    function Employes() {
-    }
-    return Employes;
-}());
-exports.Employes = Employes;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZW1wbG95ZXMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvZW1wbG95ZXMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQTtJQUFBO0lBRUEsQ0FBQztJQUFELGVBQUM7QUFBRCxDQUFDLEFBRkQsSUFFQztBQUZZLDRCQUFRIn0=
